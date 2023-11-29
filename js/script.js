2a9c5d2ec08436aad953d65772d613d3e716feb3
balbo97 <balbosauro1@gmail.com>
2023-11-29 16:54:57 +0100

    esercizio COMPLETED

--- js/script.js
+++ js/script.js
@@ -1,18 +1,30 @@
 // CREO UNA VARIABILE A CUI ASSOCIO UN ELEMENTO NELL HTML 
 const button_play = document.getElementById('play');
 
+//  creo una funzione che mi generi la singola casella della griglia 
+function createCell(x){
+    
+    const cell = document.createElement('div');
+    cell.classList.add('square') 
+
+    cell.innerText = x
+    
+    return cell;
+}
+
+
+// RECUPERO LA GRIGLIA CREANDO UNA VARIABILE 
+let grid = document.getElementById('grid');
+
 //FACCIO IN MODO CHE QUANDO CLICCO SUL BUTTON NEL HTML LA MIA FUNZIONE VENGA MESSA IN ATTO 
 button_play.addEventListener('click', function(){
-
-     //  creo una funzione che mi generi la singola casella della griglia 
-     function square (){
-
-        let cell = document.createElement('div');
-        cell.classList.add('square') 
-
-        return cell;
-     }
-
-
+    console.log('well done ')
+    
+    //CREO UN CICLO FOR
+    for (let i=0; i<100; i++){
+        
+        let square = createCell(i+1);
+        
+        grid.appendChild(square)
+    } 
 })
-  
